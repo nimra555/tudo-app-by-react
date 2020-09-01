99c91ea5db980985ac2d39470a7db46e046a43cc
nimra555 <rosegulab505@gmail.com>
2020-09-01 16:11:05 +0500

    add material-ui

--- src/Tudo.js
+++ src/Tudo.js
@@ -1,24 +1,75 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
 
-function Tudo(){
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+    },
 
-    let [tudoItem,setTudo] = useState([]); // list
-    let [tudoAdd,setAdd] = useState(""); // add item
-    let [edit,setEdit] = useState(tudoItem);
+    center: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        margin: '0 auto',
+        marginTop: 50,
+        padding: 50,
+        backgroundColor: 'rgba(47,156,132,1)',
+    },
+    head: {
+        textTransform: 'uppercase',
+        color: 'antiquewhite',
+        textShadow: '7px 5px 2px rgba(0,0,0,0.4)',
+    },
+    input: {
+        background: 'antiquewhite',
+        padding: 5,
+        border: 'none',
+        borderBottom: '3px solid rebeccapurple',
+        borderRadius: '16px  0px',
+    },
+    add:{
+    background: 'transparent',
+    color: 'antiquewhite',
+    border: '3px solid rebeccapurple',
+    borderRadius: '5px 20px',
+    fontWeight: 600,
+    },
+    list:{
+        listStyle:'none',
+    },
+    butn:{
+        backgroundColor:'rgba(47,156,132,1)',
+        border: '1px solid rebeccapurple',
+        margin: 5,
+    }
+}
+));
 
-    return(
-        <div>
-           <h1>Tudo App</h1>ulul
-           <input type="text" placeholder="Enter Tudo" onChange={(e) => {setAdd(e.target.value )}} /> <br />
-           <br />
-           <ul>
-               {tudoItem.map((tudoList,ind) =>  {return (<li key={ind}>{tudoList} </li>)})}
-               <button onClick={()=>{setTudo(tudoItem.slice(0,tudoItem.length - 1) )}}>x</button>
-        
-           <button onClick={()=>{setEdit(()=>{console.log("edit")} )}}>edit</button>
-           </ul>
-           <button onClick={()=>{setTudo([...tudoItem,tudoAdd])}}>Add Tudo</button>
-           {/* <button onClick={()=>{setTudo(tudoItem.slice(0,tudoItem.length - 1) )}}>x</button> */}
+
+
+function Tudo() {
+    const classes = useStyles();
+    let [tudoItem, setTudo] = useState([]); // list
+    let [tudoAdd, setAdd] = useState(""); // add item
+
+    return (
+        <div className={classes.root}>
+            <Paper elevation={3} className={classes.center} >
+                <h1 className={classes.head}>Tudo App</h1>
+                <input type="text" class={classes.input} placeholder="Enter Tudo" value={tudoAdd} onChange={(e) => { setAdd(e.target.value) }} />
+
+                <ul>
+                    {tudoItem.map((tudoList, ind) => {
+                        return (<li key={ind} className={classes.list}>{tudoList}
+                            <button className={classes.butn} onClick={() => { setTudo(tudoItem.slice(0, tudoItem.length - 1)) }}>x</button>
+                            <button className={classes.butn} onClick={() => { setAdd(tudoList); setTudo(tudoItem.filter(item => item !== tudoList)) }}>edit</button>
+                        </li>)
+                    })}
+                </ul>
+                <button className={classes.add} onClick={() => { setTudo([...tudoItem, tudoAdd]) }}>Add Tudo</button>
+            </Paper>
         </div>
     )
 }
